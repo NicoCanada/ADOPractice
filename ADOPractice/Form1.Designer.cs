d9fcb8a0eb6b28084c512ae8a6ebcfddb7b2e8ae
Nianguang Cai <ncai@RRC.CA>
2019-04-10 15:44:28 -0500

    Add project files.

---
+++ ADOPractice/Form1.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace ADOPractice
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.lblStockNumber = new System.Windows.Forms.Label();
+            this.cboTest = new System.Windows.Forms.ComboBox();
+            this.dgvVehicleStock = new System.Windows.Forms.DataGridView();
+            this.bindingSource1 = new System.Windows.Forms.BindingSource(this.components);
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvVehicleStock)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblStockNumber
+            // 
+            this.lblStockNumber.AutoSize = true;
+            this.lblStockNumber.Location = new System.Drawing.Point(203, 15);
+            this.lblStockNumber.Name = "lblStockNumber";
+            this.lblStockNumber.Size = new System.Drawing.Size(72, 13);
+            this.lblStockNumber.TabIndex = 0;
+            this.lblStockNumber.Text = "StockNumber";
+            // 
+            // cboTest
+            // 
+            this.cboTest.FormattingEnabled = true;
+            this.cboTest.Location = new System.Drawing.Point(36, 12);
+            this.cboTest.Name = "cboTest";
+            this.cboTest.Size = new System.Drawing.Size(121, 21);
+            this.cboTest.TabIndex = 1;
+            // 
+            // dgvVehicleStock
+            // 
+            this.dgvVehicleStock.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvVehicleStock.Location = new System.Drawing.Point(3, 42);
+            this.dgvVehicleStock.Name = "dgvVehicleStock";
+            this.dgvVehicleStock.Size = new System.Drawing.Size(1210, 396);
+            this.dgvVehicleStock.TabIndex = 2;
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(316, 10);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 3;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(426, 10);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(75, 23);
+            this.btnDelete.TabIndex = 4;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.BtnDelete_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1225, 450);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.dgvVehicleStock);
+            this.Controls.Add(this.cboTest);
+            this.Controls.Add(this.lblStockNumber);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvVehicleStock)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblStockNumber;
+        private System.Windows.Forms.ComboBox cboTest;
+        private System.Windows.Forms.DataGridView dgvVehicleStock;
+        private System.Windows.Forms.BindingSource bindingSource1;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnDelete;
+    }
+}
+
---
+++ ADOPractice/Form1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.OleDb;
+
+namespace ADOPractice
+{
+    public partial class Form1 : Form
+    {
+        OleDbConnection dbConnection;
+        OleDbCommand dbCommand;
+        OleDbDataAdapter adapter;
+        DataSet ds;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            dbConnection = new OleDbConnection("Provider=Microsoft.Jet.OLEDB.4.0;Data Source=AMDatabase.mdb");
+            dbCommand = new OleDbCommand("select * from VehicleStock;", dbConnection);
+            adapter = new OleDbDataAdapter(dbCommand);
+            ds = new DataSet();
+
+           
+            dbConnection.Open();
+            adapter.Fill(ds, "VehicleStock");
+
+            BindingSource bindingSource = new BindingSource();
+            bindingSource.DataSource = ds.Tables["VehicleStock"];
+
+
+            dgvVehicleStock.DataSource = bindingSource;
+            lblStockNumber.DataBindings.Add("Text", bindingSource, "stocknumber");
+            cboTest.DataSource = bindingSource;
+            cboTest.DisplayMember = "ID";
+            cboTest.ValueMember = "basePrice";
+
+            dbConnection.Close();
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            updateData();
+
+        }
+
+        private void updateData()
+        {
+            OleDbCommandBuilder commandBuilder = new OleDbCommandBuilder(adapter);
+            commandBuilder.GetUpdateCommand();
+            commandBuilder.GetDeleteCommand();
+            commandBuilder.GetInsertCommand();
+
+            dbConnection.Open();
+
+            adapter.Update(ds, "VehicleStock");
+
+            dbConnection.Close();
+        }
+
+        private void BtnDelete_Click(object sender, EventArgs e)
+        {
+            if(dgvVehicleStock.SelectedRows.Count > 0 && dgvVehicleStock.SelectedRows[0].Index != dgvVehicleStock.RowCount -1 )
+            {
+                foreach ( DataGridViewRow row in dgvVehicleStock.SelectedRows)
+                dgvVehicleStock.Rows.Remove(row);
+            }
+
+            updateData();
+        }
+    }
+}
